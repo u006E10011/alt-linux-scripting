5533a1c599bc1a443b237389b033d350a410caee
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-28 14:16:38 +0800

    Update firewalld-isp.sh

--- firewalld-isp.sh
+++ firewalld-isp.sh
@@ -1,17 +1,11 @@
 apt-get update && apt-get install firewalld -y
-
-exec bash
 systemctl enable --now firewalld
 
 firewall-cmd --permanent --add-masquerade
 firewall-cmd --permanent --zone=trusted --add-interface=enp7s2
 firewall-cmd --permanent --zone=trusted --add-interface=enp7s3
 
-
-#forwarding
-vim /etc/net/sysctl.conf
-:%s/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g
-:wq!
+sudo sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
 
 sysctl -p
 sysctl net.ipv4.ip_forward
