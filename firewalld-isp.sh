fda9294c6e3283774d56819ae29c1e7731a423bf
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-28 13:54:03 +0800

    Create firewalld-isp.sh

---
+++ firewalld-isp.sh
@@ -0,0 +1,17 @@
+apt-get update && apt-get install firewalld bash-completion
+
+exec bash
+systemctl enable --now firewalld
+
+firewall-cmd --permanent --add-masquerade
+firewall-cmd --permanent --zone=trusted --add-interface=enp7s2
+firewall-cmd --permanent --zone=trusted --add-interface=enp7s3
+
+
+#forwarding
+vim /etc/net/sysctl.conf
+:%s/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g
+:wq!
+
+sysctl -p
+sysctl net.ipv4.ip_forward
