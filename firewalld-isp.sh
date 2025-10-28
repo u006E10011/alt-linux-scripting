c67a841bb9e5cd97c1cbb02961bf774d544ffc82
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-28 14:27:59 +0800

    Update firewalld-isp.sh

--- firewalld-isp.sh
+++ firewalld-isp.sh
@@ -5,7 +5,7 @@ firewall-cmd --permanent --add-masquerade
 firewall-cmd --permanent --zone=trusted --add-interface=enp7s2
 firewall-cmd --permanent --zone=trusted --add-interface=enp7s3
 
-sudo sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+sudo sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/net/sysctl.conf
 
 sysctl -p
 sysctl net.ipv4.ip_forward
