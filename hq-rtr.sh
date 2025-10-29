fca833f2b3fd32ac8f55a346bff216032abf4d9c
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-29 14:08:31 +0800

    gre hq-rtr.sh

--- hq-rtr.sh
+++ hq-rtr.sh
@@ -77,6 +77,41 @@ setup_interface(){
     systemctl restart network && ip -c a
 }
 
+setup_gre(){
+        gre_default="10.10.10.1/30 peer 10.10.10.2/30"
+        local_default="172.16.1.2"
+        remote_default="172.16.2.2"
+        
+    if [[ "$auto" == "false" ]]; then
+        read -p "set gre address [$gre_default]: " gre
+        gre=${gre:-$gre_default}
+
+        read -p "set local address [$local_default]: " _local
+        _local=${_local:-$local_default}
+
+        read -p "set remote address [$remote_default]: " _remote
+        _remote=${_remote:-$remote_default}
+    else
+        gre="$gre_default"
+        _local="$local_default"
+        _remote="$remote_default"
+    fi
+
+    mkdir /etc/net/ifaces/gre1
+    echo "$gre" > /etc/net/ifaces/gre1/ipv4address
+
+     options='TUNTYPE=gre
+    TYPE=iptun
+    TUNTTL=64
+    TUNOPTIONS='\''ttl 64'\''
+    TUNMTU=1476
+    DISABLE=no'
+
+    echo "$_local" >> /etc/net/ifaces/gre1/options
+    echo "$_remote" >> /etc/net/ifaces/gre1/options
+    echo "$options" >> /etc/net/ifaces/gre1/options
+}
+
 init
 setup_firewalld
 setup_interface
