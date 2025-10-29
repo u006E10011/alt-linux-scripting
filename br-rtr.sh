b7a180c1d7bd7abb6ab4f7c5f26dab5814df17ae
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-29 14:08:16 +0800

    gre br-rtr.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -26,6 +26,7 @@ init(){
         echo "All required packages are already installed"
     fi
 }
+
 setup_firewalld(){
     systemctl enable --now firewalld
     firewall-cmd --permanent --add-masquerade
@@ -63,7 +64,44 @@ setup_interface(){
     CONFIG_IPV4=yes'
 
     echo "$options" > /etc/net/ifaces/enp7s2/options
+    setup_gre
+
     systemctl restart network && ip -c a
+}
+
+setup_gre(){
+        gre_default="10.10.10.2/30 peer 10.10.10.1/30"
+        local_default="172.16.2.2"
+        remote_default="172.16.1.2"
+        
+    if [[ "$auto" == "false" ]]; then
+        read -p "set gre address [$gre_default]: " gre
+        gre=${gre:-$gre_default}
+
+        read -p "set local address [$local_default]: " _local
+        _local=${_local:-$local_default}
+
+        read -p "set remote address [$remote_default]: " _remote
+        _remote=${_remote:-$remote_default}
+    else
+        gre="$gre_default"
+        _local="$local_default"
+        _remote="$remote_default"
+    fi
+
+    mkdir /etc/net/ifaces/gre1
+    echo "$gre" > /etc/net/ifaces/gre1/ipv4address
+
+     options='TUNTYPE=gre
+    TYPE=iptun
+    TUNTTL=64
+    TUNOPTIONS='\''ttl 64'\''
+    TUNMTU=1476
+    DISABLE=no'
+
+    echo "$_local" >> /etc/net/ifaces/gre1/options
+    echo "$_remote" >> /etc/net/ifaces/gre1/options
+    echo "$options" >> /etc/net/ifaces/gre1/options
 }
 
 init
