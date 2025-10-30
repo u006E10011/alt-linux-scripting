733e7759ac013fd0fea85496da3a1e307a3df406
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-30 14:25:38 +0800

    Update isp.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -101,12 +101,27 @@ setup_gre(){
     echo "$options" >> /etc/net/ifaces/gre1/options
 }
 
+setup_ospf()
+{
+    curl -s -o /tmp/ospf.sh "https://raw.githubusercontent.com/u006E100/alt-linux-scripting/main/ospf.sh"
+    chmod +x /tmp/ospf.sh
+    /tmp/ospf.sh --hostname "br-rtr"
+}
+
 setup_all_network(){
     setup_interface
     setup_gre
+    setup_ospf
     systemctl restart network && ip -c a
+}
+
+dispose()
+{
+    rm -rf /tmp/ospf.sh
+    rm -rf br-rtr.sh
 }
 
 init
 setup_firewalld
 setup_all_network
+dispose
