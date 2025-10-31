073cb9b8d7f83d77ba8a4fd4807649a97f5e8baf
n19 <162562221+u006E10011@users.noreply.github.com>
2025-10-31 09:28:16 +0800

    Update hq-rtr.sh

--- hq-rtr.sh
+++ hq-rtr.sh
@@ -113,14 +113,14 @@ setup_gre(){
 
 setup_ospf()
 {
-    curl -O "https://raw.githubusercontent.com/u006E100/alt-linux-scripting/main/ospf.sh"
+    curl -O "https://raw.githubusercontent.com/u006E10011/alt-linux-scripting/main/ospf.sh"
     chmod +x ospf.sh
     bash ospf.sh --hostname "hq-rtr"
 }
 
 setup_dhcp()
 {
-    curl -O "https://raw.githubusercontent.com/u006E100/alt-linux-scripting/main/dhcpd.sh"
+    curl -O "https://raw.githubusercontent.com/u006E10011/alt-linux-scripting/main/dhcpd.sh"
     chmod +x dhcpd.sh
     bash dhcpd.sh
 }
